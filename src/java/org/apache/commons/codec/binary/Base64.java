3c1f5179d186ac846e9e12ea8d7247aacb25c960
Gary D. Gregory <ggregory@apache.org>
2003-11-06 16:32:30 +0000

    Javadoc fix.
    git-svn-id: https://svn.eu.apache.org/repos/asf/jakarta/commons/proper/codec/trunk@130230 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/codec/binary/Base64.java
+++ src/java/org/apache/commons/codec/binary/Base64.java
@@ -66,16 +66,14 @@ import org.apache.commons.codec.EncoderException;
   * Provides encode/decode for RFC 2045 Base64 as 
   * defined by RFC 2045, by Freed and Borenstein.  
   *
-  * @see <a href="http://www.ietf.org/rfc/rfc2045.txt">RFC 2045</a>, by Freed and Borenstein:
-  *    Multipurpose Internet Mail Extensions (MIME) Part One: 
-  *    Format of Internet Message Bodies. Reference 1996.
+  * @see <a href="http://www.ietf.org/rfc/rfc2045.txt">RFC 2045</a>
   * @author Jeffrey Rodriguez
   * @author <a href="mailto:dlr@apache.org">Daniel Rall</a>
   * @author <a href="mailto:m.redington@ucl.ac.uk">Martin Redington</a>
   * @author <a href="mailto:ggregory@seagullsw.com">Gary Gregory</a>
   * @author Tim O'Brien
   * @since 1.0-dev
-  * @version $Id: Base64.java,v 1.12 2003/11/03 22:16:54 ggregory Exp $
+  * @version $Id: Base64.java,v 1.13 2003/11/06 16:32:30 ggregory Exp $
   */
 public class Base64 implements BinaryEncoder, BinaryDecoder {
 
