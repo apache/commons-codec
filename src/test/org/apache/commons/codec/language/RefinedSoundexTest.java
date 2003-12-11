899036a36138ab7ae810ec2b351d8226c66908bc
Gary D. Gregory <ggregory@apache.org>
2003-12-11 23:44:11 +0000

    Refactor for implementing difference() API in both Soundex and RefinedSoundex. http://nagoya.apache.org/bugzilla/show_bug.cgi?id=25243
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/codec/trunk@130271 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/commons/codec/language/RefinedSoundexTest.java
+++ src/test/org/apache/commons/codec/language/RefinedSoundexTest.java
@@ -2,109 +2,134 @@
  * ====================================================================
  * 
  * The Apache Software License, Version 1.1
- *
- * Copyright (c) 2001-2003 The Apache Software Foundation.  All rights
- * reserved.
- *
+ * 
+ * Copyright (c) 2001-2003 The Apache Software Foundation. All rights reserved.
+ * 
  * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer. 
- *
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- *
- * 3. The end-user documentation included with the redistribution,
- *    if any, must include the following acknowledgement:  
- *       "This product includes software developed by the 
- *        Apache Software Foundation (http://www.apache.org/)."
- *    Alternately, this acknowledgement may appear in the software itself,
- *    if and wherever such third-party acknowledgements normally appear.
- *
- * 4. The names "Apache", "The Jakarta Project", "Commons", and "Apache Software
- *    Foundation" must not be used to endorse or promote products derived
- *    from this software without prior written permission. For written 
- *    permission, please contact apache@apache.org.
- *
- * 5. Products derived from this software may not be called "Apache",
- *    "Apache" nor may "Apache" appear in their name without prior 
- *    written permission of the Apache Software Foundation.
- *
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
- * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
- * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
- * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
+ * modification, are permitted provided that the following conditions are met: 1.
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer. 2. Redistributions in
+ * binary form must reproduce the above copyright notice, this list of
+ * conditions and the following disclaimer in the documentation and/or other
+ * materials provided with the distribution. 3. The end-user documentation
+ * included with the redistribution, if any, must include the following
+ * acknowledgement: "This product includes software developed by the Apache
+ * Software Foundation (http://www.apache.org/)." Alternately, this
+ * acknowledgement may appear in the software itself, if and wherever such
+ * third-party acknowledgements normally appear. 4. The names "Apache", "The
+ * Jakarta Project", "Commons", and "Apache Software Foundation" must not be
+ * used to endorse or promote products derived from this software without prior
+ * written permission. For written permission, please contact
+ * apache@apache.org. 5. Products derived from this software may not be called
+ * "Apache", "Apache" nor may "Apache" appear in their name without prior
+ * written permission of the Apache Software Foundation.
+ * 
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * APACHE SOFTWARE FOUNDATION OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  * ====================================================================
- *
- * This software consists of voluntary contributions made by many
- * individuals on behalf of the Apache Software Foundation.  For more
- * information on the Apache Software Foundation, please see
- * <http://www.apache.org/>.
- *
- */ 
+ * 
+ * This software consists of voluntary contributions made by many individuals
+ * on behalf of the Apache Software Foundation. For more information on the
+ * Apache Software Foundation, please see <http://www.apache.org/> .
+ *  
+ */
 
 package org.apache.commons.codec.language;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
-
+import org.apache.commons.codec.EncoderException;
 import org.apache.commons.codec.StringEncoder;
 import org.apache.commons.codec.StringEncoderAbstractTest;
 
 /**
- * @version $Revision: 1.4 $ $Date: 2003/10/05 21:45:49 $
+ * Tests RefinedSoundex.
+ * 
+ * @version $Id: RefinedSoundexTest.java,v 1.5 2003/12/11 23:44:11 ggregory Exp $
  * @author Rodney Waldhoff
+ * @author Gary D. Gregory
  */
 public class RefinedSoundexTest extends StringEncoderAbstractTest {
-
-    public RefinedSoundexTest(String name) {
-        super(name);
-    }
 
     public static Test suite() {
         return (new TestSuite(RefinedSoundexTest.class));
     }
 
-    public void setUp() throws Exception {        
-        super.setUp();
-        _encoder = new RefinedSoundex();
+    private RefinedSoundex encoder = null;
+
+    public RefinedSoundexTest(String name) {
+        super(name);
     }
 
-    public void tearDown() throws Exception {
-        super.tearDown();
-        _encoder = null;
+    /**
+	 * @return Returns the encoder.
+	 */
+    private RefinedSoundex getEncoder() {
+        return this.encoder;
     }
 
     protected StringEncoder makeEncoder() {
         return new RefinedSoundex();
     }
 
-    // ------------------------------------------------------------------------
-
-    public void testEncode() throws Exception {
-        assertEquals("T6036084",_encoder.encode("testing"));
-        assertEquals("T60",_encoder.encode("The"));
-        assertEquals("Q503",_encoder.encode("quick"));
-        assertEquals("B1908",_encoder.encode("brown"));
-        assertEquals("F205",_encoder.encode("fox"));
-        assertEquals("J408106",_encoder.encode("jumped"));
-        assertEquals("O0209",_encoder.encode("over"));
-        assertEquals("T60",_encoder.encode("the"));
-        assertEquals("L7050",_encoder.encode("lazy"));
-        assertEquals("D6043",_encoder.encode("dogs"));
+    /**
+	 * @param encoder
+	 *                  The encoder to set.
+	 */
+    private void setEncoder(RefinedSoundex encoder) {
+        this.encoder = encoder;
     }
 
-    private RefinedSoundex _encoder = null;
+    public void setUp() throws Exception {
+        super.setUp();
+        this.setEncoder(new RefinedSoundex());
+    }
+
+    public void tearDown() throws Exception {
+        super.tearDown();
+        this.setEncoder(null);
+    }
+
+    public void testDifference() throws EncoderException {
+        // Edge cases
+        assertEquals(0, this.getEncoder().difference(null, null));
+        assertEquals(0, this.getEncoder().difference("", ""));
+        assertEquals(0, this.getEncoder().difference(" ", " "));
+        // Normal cases
+        assertEquals(6, this.getEncoder().difference("Smith", "Smythe"));
+        assertEquals(3, this.getEncoder().difference("Ann", "Andrew"));
+        assertEquals(1, this.getEncoder().difference("Margaret", "Andrew"));
+        assertEquals(1, this.getEncoder().difference("Janet", "Margaret"));
+        // Examples from
+		// http://msdn.microsoft.com/library/default.asp?url=/library/en-us/tsqlref/ts_de-dz_8co5.asp
+        assertEquals(5, this.getEncoder().difference("Green", "Greene"));
+        assertEquals(1, this.getEncoder().difference("Blotchet-Halls", "Greene"));
+        // Examples from
+		// http://msdn.microsoft.com/library/default.asp?url=/library/en-us/tsqlref/ts_setu-sus_3o6w.asp
+        assertEquals(6, this.getEncoder().difference("Smith", "Smythe"));
+        assertEquals(8, this.getEncoder().difference("Smithers", "Smythers"));
+        assertEquals(5, this.getEncoder().difference("Anothers", "Brothers"));
+    }
+
+    public void testEncode() throws EncoderException {
+        assertEquals("T6036084", this.getEncoder().encode("testing"));
+        assertEquals("T6036084", this.getEncoder().encode("TESTING"));
+        assertEquals("T60", this.getEncoder().encode("The"));
+        assertEquals("Q503", this.getEncoder().encode("quick"));
+        assertEquals("B1908", this.getEncoder().encode("brown"));
+        assertEquals("F205", this.getEncoder().encode("fox"));
+        assertEquals("J408106", this.getEncoder().encode("jumped"));
+        assertEquals("O0209", this.getEncoder().encode("over"));
+        assertEquals("T60", this.getEncoder().encode("the"));
+        assertEquals("L7050", this.getEncoder().encode("lazy"));
+        assertEquals("D6043", this.getEncoder().encode("dogs"));
+    }
 }
