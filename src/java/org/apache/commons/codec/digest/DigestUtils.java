90cb58217ebc628734ebbd4ca84e9eea9305dd77
Henri Yandell <bayard@apache.org>
2004-03-12 05:08:27 +0000

    removing tabs.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/codec/trunk@130306 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/codec/digest/DigestUtils.java
+++ src/java/org/apache/commons/codec/digest/DigestUtils.java
@@ -54,100 +54,100 @@ public class DigestUtils {
         return getDigest("MD5");
     }
 
-	/**
-	 * Returns an SHA digest.
-	 *
-	 * @return An SHA digest instance.
+    /**
+     * Returns an SHA digest.
+     *
+     * @return An SHA digest instance.
      * @throws RuntimeException when a {@link java.security.NoSuchAlgorithmException} is caught,
-	 */
-	private static MessageDigest getShaDigest() {
+     */
+    private static MessageDigest getShaDigest() {
         return getDigest("SHA");
-	}
+    }
 
-	/**
-	 * Calculates the MD5 digest and returns the value as a 16 element 
-	 * <code>byte[]</code>.
-	 *
-	 * @param data Data to digest
-	 * @return MD5 digest
-	 */
-	public static byte[] md5(byte[] data) {
-		return getMd5Digest().digest(data);
-	}
+    /**
+     * Calculates the MD5 digest and returns the value as a 16 element 
+     * <code>byte[]</code>.
+     *
+     * @param data Data to digest
+     * @return MD5 digest
+     */
+    public static byte[] md5(byte[] data) {
+        return getMd5Digest().digest(data);
+    }
 
-	/**
-	 * Calculates the MD5 digest and returns the value as a 16 element 
-	 * <code>byte[]</code>.
-	 *
-	 * @param data Data to digest
-	 * @return MD5 digest
-	 */
-	public static byte[] md5(String data) {
-		return md5(data.getBytes());
-	}
+    /**
+     * Calculates the MD5 digest and returns the value as a 16 element 
+     * <code>byte[]</code>.
+     *
+     * @param data Data to digest
+     * @return MD5 digest
+     */
+    public static byte[] md5(String data) {
+        return md5(data.getBytes());
+    }
 
-	/**
-	 * Calculates the MD5 digest and returns the value as a 32 character 
-	 * hex string.
-	 *
-	 * @param data Data to digest
-	 * @return MD5 digest as a hex string
-	 */
-	public static String md5Hex(byte[] data) {
-		return new String(Hex.encodeHex(md5(data)));
-	}
+    /**
+     * Calculates the MD5 digest and returns the value as a 32 character 
+     * hex string.
+     *
+     * @param data Data to digest
+     * @return MD5 digest as a hex string
+     */
+    public static String md5Hex(byte[] data) {
+        return new String(Hex.encodeHex(md5(data)));
+    }
 
-	/**
-	 * Calculates the MD5 digest and returns the value as a 32 character 
-	 * hex string.
-	 *
-	 * @param data Data to digest
-	 * @return MD5 digest as a hex string
-	 */
-	public static String md5Hex(String data) {
-		return new String(Hex.encodeHex(md5(data)));
-	}
+    /**
+     * Calculates the MD5 digest and returns the value as a 32 character 
+     * hex string.
+     *
+     * @param data Data to digest
+     * @return MD5 digest as a hex string
+     */
+    public static String md5Hex(String data) {
+        return new String(Hex.encodeHex(md5(data)));
+    }
 
-	/**
-	 * Calculates the SHA digest and returns the value as a 
-	 * <code>byte[]</code>.
-	 *
-	 * @param data Data to digest
-	 * @return SHA digest
-	 */
-	public static byte[] sha(byte[] data) {
-		return getShaDigest().digest(data);
-	}
+    /**
+     * Calculates the SHA digest and returns the value as a 
+     * <code>byte[]</code>.
+     *
+     * @param data Data to digest
+     * @return SHA digest
+     */
+    public static byte[] sha(byte[] data) {
+        return getShaDigest().digest(data);
+    }
 
-	/**
-	 * Calculates the SHA digest and returns the value as a 
-	 * <code>byte[]</code>.
-	 *
-	 * @param data Data to digest
-	 * @return SHA digest
-	 */
-	public static byte[] sha(String data) {
-		return sha(data.getBytes());
-	}
+    /**
+     * Calculates the SHA digest and returns the value as a 
+     * <code>byte[]</code>.
+     *
+     * @param data Data to digest
+     * @return SHA digest
+     */
+    public static byte[] sha(String data) {
+        return sha(data.getBytes());
+    }
 
-	/**
+    /**
      * Calculates the SHA digest and returns the value as a hex string.
      *
      * @param data Data to digest
      * @return SHA digest as a hex string
      */
-	public static String shaHex(byte[] data) {
-		return new String(Hex.encodeHex(sha(data)));
-	}
+    public static String shaHex(byte[] data) {
+        return new String(Hex.encodeHex(sha(data)));
+    }
 
-	/**
-	 * Calculates the SHA digest and returns the value as a hex string.
-	 *
-	 * @param data Data to digest
-	 * @return SHA digest as a hex string
-	 */
-	public static String shaHex(String data) {
-		return new String(Hex.encodeHex(sha(data)));
-	}
+    /**
+     * Calculates the SHA digest and returns the value as a hex string.
+     *
+     * @param data Data to digest
+     * @return SHA digest as a hex string
+     */
+    public static String shaHex(String data) {
+        return new String(Hex.encodeHex(sha(data)));
+    }
 
 }
