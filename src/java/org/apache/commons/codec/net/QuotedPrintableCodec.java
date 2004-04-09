5e959f808b20dd5577fe9e74ad840861cd2c23a6
Gary D. Gregory <ggregory@apache.org>
2004-04-09 22:21:07 +0000

    Fix headers.
    git-svn-id: https://svn.eu.apache.org/repos/asf/jakarta/commons/proper/codec/trunk@130351 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/codec/net/QuotedPrintableCodec.java
+++ src/java/org/apache/commons/codec/net/QuotedPrintableCodec.java
@@ -1,15 +1,18 @@
 /*
  * Copyright 2001-2004 The Apache Software Foundation.
  * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  * 
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  * 
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
- */
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */ 
 
 package org.apache.commons.codec.net;
 
@@ -51,7 +54,7 @@ import org.apache.commons.codec.StringEncoder;
  * 
  * @author Apache Software Foundation
  * @since 1.3
- * @version $Id: QuotedPrintableCodec.java,v 1.6 2004/03/29 07:59:00 ggregory Exp $
+ * @version $Id: QuotedPrintableCodec.java,v 1.7 2004/04/09 22:21:07 ggregory Exp $
  */
 public class QuotedPrintableCodec implements BinaryEncoder, BinaryDecoder, StringEncoder, StringDecoder {
     /**
