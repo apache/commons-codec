e133c23d6b8eb548fa3852b52740b523dfa2bad3
Gary D. Gregory <ggregory@apache.org>
2004-04-09 22:21:07 +0000

    Fix headers.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/codec/trunk@130351 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/codec/net/BCodec.java
+++ src/java/org/apache/commons/codec/net/BCodec.java
@@ -1,15 +1,18 @@
 /*
  * Copyright 2001-2004 The Apache Software Foundation.
  * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  * 
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  * 
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
- */
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */ 
 
 package org.apache.commons.codec.net;
 
@@ -37,7 +40,7 @@ import org.apache.commons.codec.binary.Base64;
  * 
  * @author Apache Software Foundation
  * @since 1.3
- * @version $Id: BCodec.java,v 1.2 2004/04/09 21:45:47 ggregory Exp $
+ * @version $Id: BCodec.java,v 1.3 2004/04/09 22:21:07 ggregory Exp $
  */
 public class BCodec extends RFC1522Codec implements StringEncoder, StringDecoder {
     /**
