9c0cabead7cf075308b11362172ae1a48d41321c
Henri Yandell <bayard@apache.org>
2008-04-27 00:33:06 +0000

    Modifying the unit test for CODEC-57 to test for empty string, and added a note to the Metaphone javadoc that it does not match the PHP or Perl variants
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/codec/trunk@651873 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/codec/language/Metaphone.java
+++ src/java/org/apache/commons/codec/language/Metaphone.java
@@ -30,6 +30,11 @@ import org.apache.commons.codec.StringEncoder;
  * <CITE>Hanging on the Metaphone</CITE> by <CITE>Lawrence Philips</CITE> in <CITE>Computer Language of Dec. 1990, p
  * 39.</CITE>
  * </p>
+ * <p>
+ * Note, that this does not match the algorithm that ships with PHP, or the algorithm 
+ * found in the Perl <a href="http://search.cpan.org/~mschwern/Text-Metaphone-1.96/Metaphone.pm">Text:Metaphone-1.96</a>.
+ * They have had undocumented changes from the originally published algorithm. 
+ * </p>
  * 
  * @author Apache Software Foundation
  * @version $Id$
