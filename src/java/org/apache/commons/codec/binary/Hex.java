5fcf0a6efba9c25d69331a803777a9270e55980c
Gary D. Gregory <ggregory@apache.org>
2009-01-20 20:53:04 +0000

    [CODEC-74] Allow for uppercase letters as the output of Hex.encodeHex()
    git-svn-id: https://svn.eu.apache.org/repos/asf/commons/proper/codec/trunk@736102 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/codec/binary/Hex.java
+++ src/java/org/apache/commons/codec/binary/Hex.java
@@ -34,9 +34,17 @@ public class Hex implements BinaryEncoder, BinaryDecoder {
     /** 
      * Used to build output as Hex 
      */
-    private static final char[] DIGITS = {
+    private static final char[] DIGITS_LOWER = {
         '0', '1', '2', '3', '4', '5', '6', '7',
            '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'
+    };
+
+    /** 
+     * Used to build output as Hex 
+     */
+    private static final char[] DIGITS_UPPER = {
+        '0', '1', '2', '3', '4', '5', '6', '7',
+           '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'
     };
 
     /**
@@ -96,22 +104,52 @@ public class Hex implements BinaryEncoder, BinaryDecoder {
      * given byte.
      * 
      * @param data
-     *                  a byte[] to convert to Hex characters
+     *            a byte[] to convert to Hex characters
      * @return A char[] containing hexadecimal characters
      */
     public static char[] encodeHex(byte[] data) {
+        return encodeHex(data, true);
+    }
+
+    /**
+     * Converts an array of bytes into an array of characters representing the hexadecimal values of each byte in order.
+     * The returned array will be double the length of the passed array, as it takes two characters to represent any
+     * given byte.
+     * 
+     * @param data
+     *            a byte[] to convert to Hex characters
+     * @param toLowerCase
+     *            <code>true</code> converts to lowercase, <code>false</code> to uppercase
+     * @return A char[] containing hexadecimal characters
+     */
+    public static char[] encodeHex(byte[] data, boolean toLowerCase) {
+        return encodeHex(data, toLowerCase ? DIGITS_LOWER : DIGITS_UPPER);
+    }
+
+    /**
+     * Converts an array of bytes into an array of characters representing the hexadecimal values of each byte in order.
+     * The returned array will be double the length of the passed array, as it takes two characters to represent any
+     * given byte.
+     * 
+     * @param data
+     *            a byte[] to convert to Hex characters
+     * @param toDigits
+     *            the output alphabet
+     * @return A char[] containing hexadecimal characters
+     */
+    protected static char[] encodeHex(byte[] data, char[] toDigits) {
 
         int l = data.length;
 
-           char[] out = new char[l << 1];
+        char[] out = new char[l << 1];
 
-           // two characters form the hex value.
-           for (int i = 0, j = 0; i < l; i++) {
-               out[j++] = DIGITS[(0xF0 & data[i]) >>> 4 ];
-               out[j++] = DIGITS[ 0x0F & data[i] ];
-           }
+        // two characters form the hex value.
+        for (int i = 0, j = 0; i < l; i++) {
+            out[j++] = toDigits[(0xF0 & data[i]) >>> 4];
+            out[j++] = toDigits[0x0F & data[i]];
+        }
 
-           return out;
+        return out;
     }
 
     /**
