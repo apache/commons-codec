9f243610fca08de0998ae66c55bb79eed3c45720
Gary D. Gregory <ggregory@apache.org>
2009-07-28 17:12:35 +0000

    @since 1.4.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/codec/trunk@798613 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/commons/codec/net/UtilsTest.java
+++ src/test/org/apache/commons/codec/net/UtilsTest.java
@@ -27,6 +27,7 @@ import junit.framework.TestCase;
  * 
  * @author <a href="mailto:ggregory@seagullsw.com">Gary Gregory</a>
  * @version $Id$
+ * @since 1.4
  */
 public class UtilsTest extends TestCase {
 
