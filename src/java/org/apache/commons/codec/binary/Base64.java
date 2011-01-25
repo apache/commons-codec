a791fb32945054c162624fa1aa45122b4034b6ab
Sebastian Bazley <sebb@apache.org>
2011-01-25 19:41:08 +0000

    Don't use the now deprecated isArrayByteBase64 method, use its replacement isBase64
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/codec/trunk@1063421 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/codec/binary/Base64.java
+++ src/java/org/apache/commons/codec/binary/Base64.java
@@ -600,7 +600,7 @@ public class Base64 implements BinaryEncoder, BinaryDecoder {
      *  @since 1.5
      */
     public static boolean isBase64(String base64) {
-        return isArrayByteBase64(StringUtils.getBytesUtf8(base64));
+        return isBase64(StringUtils.getBytesUtf8(base64));
     }
 
     /**
@@ -611,7 +611,7 @@ public class Base64 implements BinaryEncoder, BinaryDecoder {
      *            byte array to test
      * @return <code>true</code> if all bytes are valid characters in the Base64 alphabet or if the byte array is empty;
      *         <code>false</code>, otherwise
-     * @deprecated Use {@link #isBase64(byte[])}
+     * @deprecated 1.5 Use {@link #isBase64(byte[])}
      */
     public static boolean isArrayByteBase64(byte[] arrayOctet) {
         return isBase64(arrayOctet);
