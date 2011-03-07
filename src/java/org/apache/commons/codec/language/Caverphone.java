7656a09e8587355ee142e730f5df99ce72b382e5
Gary D. Gregory <ggregory@apache.org>
2011-03-07 01:18:12 +0000

    [CODEC-118] Split Caverphone class into two classes for Caverphone 1.0 and 2.0.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/codec/trunk@1078646 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/codec/language/Caverphone.java
+++ src/java/org/apache/commons/codec/language/Caverphone.java
@@ -21,7 +21,7 @@ import org.apache.commons.codec.EncoderException;
 import org.apache.commons.codec.StringEncoder;
 
 /**
- * Encodes a string into a Caverphone value.
+ * Encodes a string into a Caverphone 2.0 value. Delegate to a {@link Caverphone2} instance.
  * 
  * This is an algorithm created by the Caversham Project at the University of Otago. It implements the Caverphone 2.0
  * algorithm:
@@ -31,8 +31,14 @@ import org.apache.commons.codec.StringEncoder;
  * @see <a href="http://en.wikipedia.org/wiki/Caverphone">Wikipedia - Caverphone</a>
  * @see <a href="http://caversham.otago.ac.nz/files/working/ctp150804.pdf">Caverphone 2.0 specification</a>
  * @since 1.4
+ * @deprecated Replaced by {@link Caverphone2}, will be removed in the next version.
  */
 public class Caverphone implements StringEncoder {
+
+    /**
+     * Delegate to a {@link Caverphone2} instance to avoid code duplication.
+     */
+    final private Caverphone2 encoder = new Caverphone2();
 
     /**
      * Creates an instance of the Caverphone encoder
@@ -43,143 +49,55 @@ public class Caverphone implements StringEncoder {
 
     /**
      * Encodes the given String into a Caverphone value.
-     *
-     * @param txt String the source string
+     * 
+     * @param source
+     *            String the source string
      * @return A caverphone code for the given String
      */
-    public String caverphone(String txt) {
-        // NOTE: Version 1.0 of Caverphone is easily derivable from this code 
-        // by commenting out the 2.0 lines and adding in the 1.0 lines
-
-        if( txt == null || txt.length() == 0 ) {
-            return "1111111111";
-        }
-
-        // 1. Convert to lowercase
-        txt = txt.toLowerCase(java.util.Locale.ENGLISH);
-
-        // 2. Remove anything not A-Z
-        txt = txt.replaceAll("[^a-z]", "");
-
-        // 2.5. Remove final e
-        txt = txt.replaceAll("e$", "");             // 2.0 only
-
-        // 3. Handle various start options
-        txt = txt.replaceAll("^cough", "cou2f");
-        txt = txt.replaceAll("^rough", "rou2f");
-        txt = txt.replaceAll("^tough", "tou2f");
-        txt = txt.replaceAll("^enough", "enou2f");  // 2.0 only
-        txt = txt.replaceAll("^trough", "trou2f");  // 2.0 only - note the spec says ^enough here again, c+p error I assume
-        txt = txt.replaceAll("^gn", "2n");
-
-        // End 
-        txt = txt.replaceAll("mb$", "m2");
-
-        // 4. Handle replacements
-        txt = txt.replaceAll("cq", "2q");
-        txt = txt.replaceAll("ci", "si");
-        txt = txt.replaceAll("ce", "se");
-        txt = txt.replaceAll("cy", "sy");
-        txt = txt.replaceAll("tch", "2ch");
-        txt = txt.replaceAll("c", "k");
-        txt = txt.replaceAll("q", "k");
-        txt = txt.replaceAll("x", "k");
-        txt = txt.replaceAll("v", "f");
-        txt = txt.replaceAll("dg", "2g");
-        txt = txt.replaceAll("tio", "sio");
-        txt = txt.replaceAll("tia", "sia");
-        txt = txt.replaceAll("d", "t");
-        txt = txt.replaceAll("ph", "fh");
-        txt = txt.replaceAll("b", "p");
-        txt = txt.replaceAll("sh", "s2");
-        txt = txt.replaceAll("z", "s");
-        txt = txt.replaceAll("^[aeiou]", "A");
-        txt = txt.replaceAll("[aeiou]", "3");
-        txt = txt.replaceAll("j", "y");        // 2.0 only
-        txt = txt.replaceAll("^y3", "Y3");     // 2.0 only
-        txt = txt.replaceAll("^y", "A");       // 2.0 only
-        txt = txt.replaceAll("y", "3");        // 2.0 only
-        txt = txt.replaceAll("3gh3", "3kh3");
-        txt = txt.replaceAll("gh", "22");
-        txt = txt.replaceAll("g", "k");
-        txt = txt.replaceAll("s+", "S");
-        txt = txt.replaceAll("t+", "T");
-        txt = txt.replaceAll("p+", "P");
-        txt = txt.replaceAll("k+", "K");
-        txt = txt.replaceAll("f+", "F");
-        txt = txt.replaceAll("m+", "M");
-        txt = txt.replaceAll("n+", "N");
-        txt = txt.replaceAll("w3", "W3");
-        //txt = txt.replaceAll("wy", "Wy");    // 1.0 only
-        txt = txt.replaceAll("wh3", "Wh3");
-        txt = txt.replaceAll("w$", "3");       // 2.0 only
-        //txt = txt.replaceAll("why", "Why");  // 1.0 only
-        txt = txt.replaceAll("w", "2");
-        txt = txt.replaceAll("^h", "A");
-        txt = txt.replaceAll("h", "2");
-        txt = txt.replaceAll("r3", "R3");
-        txt = txt.replaceAll("r$", "3");       // 2.0 only
-        //txt = txt.replaceAll("ry", "Ry");    // 1.0 only
-        txt = txt.replaceAll("r", "2");
-        txt = txt.replaceAll("l3", "L3");
-        txt = txt.replaceAll("l$", "3");       // 2.0 only
-        //txt = txt.replaceAll("ly", "Ly");    // 1.0 only
-        txt = txt.replaceAll("l", "2");
-        //txt = txt.replaceAll("j", "y");      // 1.0 only
-        //txt = txt.replaceAll("y3", "Y3");    // 1.0 only
-        //txt = txt.replaceAll("y", "2");      // 1.0 only
-
-        // 5. Handle removals
-        txt = txt.replaceAll("2", "");
-        txt = txt.replaceAll("3$", "A");       // 2.0 only
-        txt = txt.replaceAll("3", "");
-
-        // 6. put ten 1s on the end
-        txt = txt + "111111" + "1111";        // 1.0 only has 6 1s
-
-        // 7. take the first six characters as the code
-        return txt.substring(0, 10);          // 1.0 truncates to 6
+    public String caverphone(String source) {
+        return this.encoder.encode(source);
     }
 
     /**
-     * Encodes an Object using the caverphone algorithm.  This method
-     * is provided in order to satisfy the requirements of the
-     * Encoder interface, and will throw an EncoderException if the
-     * supplied object is not of type java.lang.String.
-     *
-     * @param pObject Object to encode
-     * @return An object (or type java.lang.String) containing the 
-     *         caverphone code which corresponds to the String supplied.
-     * @throws EncoderException if the parameter supplied is not
-     *                          of type java.lang.String
+     * Encodes an Object using the caverphone algorithm. This method is provided in order to satisfy the requirements of
+     * the Encoder interface, and will throw an EncoderException if the supplied object is not of type java.lang.String.
+     * 
+     * @param pObject
+     *            Object to encode
+     * @return An object (or type java.lang.String) containing the caverphone code which corresponds to the String
+     *         supplied.
+     * @throws EncoderException
+     *             if the parameter supplied is not of type java.lang.String
      */
     public Object encode(Object pObject) throws EncoderException {
         if (!(pObject instanceof String)) {
-            throw new EncoderException("Parameter supplied to Caverphone encode is not of type java.lang.String"); 
+            throw new EncoderException("Parameter supplied to Caverphone encode is not of type java.lang.String");
         }
-        return caverphone((String) pObject);
+        return this.caverphone((String) pObject);
     }
 
     /**
-     * Encodes a String using the Caverphone algorithm. 
-     *
-     * @param pString String object to encode
+     * Encodes a String using the Caverphone algorithm.
+     * 
+     * @param pString
+     *            String object to encode
      * @return The caverphone code corresponding to the String supplied
      */
     public String encode(String pString) {
-        return caverphone(pString);   
+        return this.caverphone(pString);
     }
 
     /**
      * Tests if the caverphones of two strings are identical.
-     *
-     * @param str1 First of two strings to compare
-     * @param str2 Second of two strings to compare
-     * @return <code>true</code> if the caverphones of these strings are identical, 
-     *        <code>false</code> otherwise.
+     * 
+     * @param str1
+     *            First of two strings to compare
+     * @param str2
+     *            Second of two strings to compare
+     * @return <code>true</code> if the caverphones of these strings are identical, <code>false</code> otherwise.
      */
     public boolean isCaverphoneEqual(String str1, String str2) {
-        return caverphone(str1).equals(caverphone(str2));
+        return this.caverphone(str1).equals(this.caverphone(str2));
     }
 
 }
---
+++ src/test/org/apache/commons/codec/language/Caverphone2Test.java
@@ -0,0 +1,39 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.commons.codec.language;
+
+import org.apache.commons.codec.StringEncoder;
+
+/**
+ * Tests Caverphone2.
+ * 
+ * @author Apache Software Foundation
+ * @version $Id: CaverphoneTest.java 1075947 2011-03-01 17:56:14Z ggregory $
+ * @since 1.5
+ */
+public class Caverphone2Test extends CaverphoneTest {
+
+    public Caverphone2Test(String name) {
+        super(name);
+    }
+
+    protected StringEncoder createStringEncoder() {
+        return new Caverphone2();
+    }
+
+}
