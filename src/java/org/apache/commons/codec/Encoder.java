1440a376ccd084fa50c7e6f7adf251bb0d489512
Gary D. Gregory <ggregory@apache.org>
2011-05-20 05:15:19 +0000

    Fix Javadoc typos.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/codec/trunk@1125237 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/codec/Encoder.java
+++ src/java/org/apache/commons/codec/Encoder.java
@@ -20,7 +20,7 @@ package org.apache.commons.codec;
 /**
  * <p>Provides the highest level of abstraction for Encoders.
  * This is the sister interface of {@link Decoder}.  Every implementation of
- * Encoder provides this common generic interface whic allows a user to pass a 
+ * Encoder provides this common generic interface which allows a user to pass a 
  * generic Object to any Encoder implementation in the codec package.</p>
  *
  * @author Apache Software Foundation
