d44dc8b805a7b8a99b264c5bd3f7acf8371247ef
Gary D. Gregory <ggregory@apache.org>
2011-11-13 21:36:18 +0000

    Add back ctor for 1.6 to keep binary compatibility.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/codec/trunk@1201520 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/codec/StringEncoderComparator.java
+++ src/main/java/org/apache/commons/codec/StringEncoderComparator.java
@@ -35,6 +35,16 @@ public class StringEncoderComparator implements Comparator {
     private final StringEncoder stringEncoder;
 
     /**
+     * Constructs a new instance.
+     * 
+     * @deprecated Creating an instance without a {@link StringEncoder} leads to a {@link NullPointerException}. Will be
+     *             removed in 2.0.
+     */
+    public StringEncoderComparator() {
+        this.stringEncoder = null; // Trying to use this will cause things to break
+    }
+
+    /**
      * Constructs a new instance with the given algorithm.
      * 
      * @param stringEncoder
