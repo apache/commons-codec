61dcb07513fbb32c90e5ae90fa376bdf7705e2db
Gary D. Gregory <ggregory@apache.org>
2012-03-18 19:17:45 +0000

    Organize imports.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/codec/trunk@1302200 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/commons/codec/binary/Base64InputStreamTest.java
+++ src/test/java/org/apache/commons/codec/binary/Base64InputStreamTest.java
@@ -24,7 +24,6 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
