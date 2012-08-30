17a88b3a7558387d75d27e22771993a45969944f
Gary D. Gregory <ggregory@apache.org>
2012-08-30 14:16:13 +0000

    Add toString() method useful for debugging.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/codec/trunk@1378947 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/codec/binary/BaseNCodec.java
+++ src/main/java/org/apache/commons/codec/binary/BaseNCodec.java
@@ -89,6 +89,18 @@ public abstract class BaseNCodec implements BinaryEncoder, BinaryDecoder {
 
         Context() {
         }
+
+        /**
+         * Returns a String useful for debugging (especially within a debugger.)
+         * 
+         * @return a String useful for debugging.
+         */
+        public String toString() {
+            return String
+                    .format("%s[buffer=%s, currentLinePos=%s, eof=%s, ibitWorkArea=%s, lbitWorkArea=%s, modulus=%s, pos=%s, readPos=%s]",
+                            this.getClass().getSimpleName(), buffer, currentLinePos, eof, ibitWorkArea, lbitWorkArea,
+                            modulus, pos, readPos);
+        }
     }
 
     /**
