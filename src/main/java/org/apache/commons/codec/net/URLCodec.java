8cfa4882a175d3e1327098c898448a701fbb2375
Gary D. Gregory <ggregory@apache.org>
2013-03-30 05:22:40 +0000

    Fix Javadoc @see reference.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/codec/trunk@1462705 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/codec/net/URLCodec.java
+++ src/main/java/org/apache/commons/codec/net/URLCodec.java
@@ -39,7 +39,7 @@ import org.apache.commons.codec.binary.StringUtils;
  * <p>
  * This class is immutable and thread-safe.
  *
- * @see "<a href='http://www.w3.org/TR/html4/interact/forms.html#h-17.13.4.1'>Chapter 17.13.4 'Form content types'</a>"
+ * @see <a href="http://www.w3.org/TR/html4/interact/forms.html#h-17.13.4.1">Chapter 17.13.4 Form content types</a>
  *           of the <a href="http://www.w3.org/TR/html4/">HTML 4.01 Specification<a>
  *
  * @since 1.2
