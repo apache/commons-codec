9ff186bd4faaa784b0ba9bdd8980f9f9d41bd8f8
Gary D. Gregory <ggregory@apache.org>
2013-04-06 04:03:12 +0000

    Fix Checkstyle.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/codec/trunk@1465182 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/codec/binary/BaseNCodec.java
+++ src/main/java/org/apache/commons/codec/binary/BaseNCodec.java
@@ -100,8 +100,8 @@ public abstract class BaseNCodec implements BinaryEncoder, BinaryDecoder {
         @SuppressWarnings("boxing") // OK to ignore boxing here
         @Override
         public String toString() {
-            return String.format("%s[buffer=%s, currentLinePos=%s, eof=%s, ibitWorkArea=%s, lbitWorkArea=%s, "
-                    + "modulus=%s, pos=%s, readPos=%s]", this.getClass().getSimpleName(), Arrays.toString(buffer),
+            return String.format("%s[buffer=%s, currentLinePos=%s, eof=%s, ibitWorkArea=%s, lbitWorkArea=%s, " +
+                    "modulus=%s, pos=%s, readPos=%s]", this.getClass().getSimpleName(), Arrays.toString(buffer),
                     currentLinePos, eof, ibitWorkArea, lbitWorkArea, modulus, pos, readPos);
         }
     }
