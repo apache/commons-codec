9e7c59dfd25b087cc56e13e406ab1124b0d28829
Gary D. Gregory <ggregory@apache.org>
2013-04-30 12:32:24 +0000

    [CODEC-170] Link broken in Metaphone Javadoc.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/codec/trunk@1477592 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/codec/language/Metaphone.java
+++ src/main/java/org/apache/commons/codec/language/Metaphone.java
@@ -29,8 +29,13 @@ import org.apache.commons.codec.StringEncoder;
  * <CITE>Hanging on the Metaphone</CITE> by <CITE>Lawrence Philips</CITE> in <CITE>Computer Language of Dec. 1990,
  * p 39.</CITE>
  * <p>
- * Note, that this does not match the algorithm that ships with PHP, or the algorithm found in the Perl
- * <a href="http://search.cpan.org/~mschwern/Text-Metaphone-1.96/Metaphone.pm">Text:Metaphone-1.96</a>.
+ * Note, that this does not match the algorithm that ships with PHP, or the algorithm found in the Perl implemenations:
+ * </p>
+ * <ul>
+ * <li><a href="http://search.cpan.org/~mschwern/Text-Metaphone-1.96/Metaphone.pm">Text:Metaphone-1.96</a> (broken link 4/30/2013) </li>
+ * <li><a href="https://metacpan.org/source/MSCHWERN/Text-Metaphone-1.96//Metaphone.pm">Text:Metaphone-1.96</a> (link checked 4/30/2013) </li>
+ * </ul>
+ * <p>
  * They have had undocumented changes from the originally published algorithm.
  * For more information, see <a href="https://issues.apache.org/jira/browse/CODEC-57">CODEC-57</a>.
  * <p>
