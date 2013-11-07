8b0318c9dded3ef6306d3ee5904721149b7981e1
Gary D. Gregory <ggregory@apache.org>
2013-11-07 19:37:16 +0000

    [CODEC-174] Improve performance of Beider Morse encoder. Add a performance test.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/codec/trunk@1539776 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/java/org/apache/commons/codec/language/bm/PhoneticEnginePerformanceTest.java
@@ -0,0 +1,54 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.commons.codec.language.bm;
+
+import org.junit.Test;
+
+/**
+ * Tests performance for {@link PhoneticEngine}.
+ * <p>
+ * See <a href="https://issues.apache.org/jira/browse/CODEC-174">[CODEC-174] Improve performance of Beider Morse encoder</a>.
+ * </p>
+ * <p>
+ * Results for November 7, 2013, SVN revision 1539678.
+ * </p>
+ * <ol>
+ * <li>Time for encoding 80,000 times the input 'Angelo': 33,039 millis.</li>
+ * <li>Time for encoding 80,000 times the input 'Angelo': 32,297 millis.</li>
+ * <li>Time for encoding 80,000 times the input 'Angelo': 32,857 millis.</li>
+ * <li>Time for encoding 80,000 times the input 'Angelo': 31,561 millis.</li>
+ * <li>Time for encoding 80,000 times the input 'Angelo': 32,665 millis.</li>
+ * <li>Time for encoding 80,000 times the input 'Angelo': 32,215 millis.</li>
+ * </ol>
+ */
+public class PhoneticEnginePerformanceTest {
+
+    private static final int LOOP = 80000;
+
+    @Test
+    public void test() {
+        PhoneticEngine engine = new PhoneticEngine(NameType.GENERIC, RuleType.APPROX, true);
+        final String input = "Angelo";
+        final long startMillis = System.currentTimeMillis();
+        for (int i = 0; i < LOOP; i++) {
+            engine.encode(input);
+        }
+        final long totalMillis = System.currentTimeMillis() - startMillis;
+        System.out.println(String.format("Time for encoding %,d times the input '%s': %,d millis.", LOOP, input,
+                totalMillis));
+    }
+}
