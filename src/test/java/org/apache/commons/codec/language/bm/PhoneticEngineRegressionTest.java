1189e1b201e0234b54df551804098df18bd9f445
Gary D. Gregory <ggregory@apache.org>
2014-06-18 19:57:30 +0000

    [CODEC-187] Beider Morse Phonetic Matching producing incorrect tokens. Revert changes from r1603573.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/codec/trunk@1603614 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/commons/codec/language/bm/PhoneticEngineRegressionTest.java
+++ src/test/java/org/apache/commons/codec/language/bm/PhoneticEngineRegressionTest.java
@@ -186,10 +186,6 @@ public class PhoneticEngineRegressionTest {
         args.put("nameType", "GENERIC");
         args.put("ruleType", "APPROX");
         assertEquals(encode(args, true, "abram"), "Ybram|Ybrom|abram|abran|abrom|abron|avram|avrom|obram|obran|obrom|obron|ovram|ovrom");
-
-        args.put("nameType", "ASHKENAZI");
-        args.put("ruleType", "APPROX");
-        assertEquals(encode(args, true, "abram"), "Ybram|Ybrom|abram|abrom|avram|avrom|imbram|imbrom|obram|obrom|ombram|ombrom|ovram|ovrom");
     }
 
     /**
