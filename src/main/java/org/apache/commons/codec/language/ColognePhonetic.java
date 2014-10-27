d5949d0969cec0d2f036325ed9f0645f69a48c71
Gary D. Gregory <ggregory@apache.org>
2014-10-27 00:38:23 +0000

    Fix Javadoc 8 error.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/codec/trunk@1634415 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/codec/language/ColognePhonetic.java
+++ src/main/java/org/apache/commons/codec/language/ColognePhonetic.java
@@ -27,10 +27,11 @@ import org.apache.commons.codec.StringEncoder;
  * <p>
  * Implements the <a href="http://de.wikipedia.org/wiki/K%C3%B6lner_Phonetik">K&ouml;lner Phonetik</a>
  * (Cologne Phonetic) algorithm issued by Hans Joachim Postel in 1969.
+ * </p>
  * <p>
  * The <i>K&ouml;lner Phonetik</i> is a phonetic algorithm which is optimized for the German language.
  * It is related to the well-known soundex algorithm.
- * <p>
+ * </p>
  *
  * <h2>Algorithm</h2>
  *
@@ -170,7 +171,9 @@ import org.apache.commons.codec.StringEncoder;
  *
  * </ul>
  *
+ * <p>
  * This class is thread-safe.
+ * </p>
  *
  * @see <a href="http://de.wikipedia.org/wiki/K%C3%B6lner_Phonetik">Wikipedia (de): K&ouml;lner Phonetik (in German)</a>
  * @since 1.5
