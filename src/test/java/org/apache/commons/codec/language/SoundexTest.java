8fc193527df27beeeb638e6e812e8c4df6c72f46
Gary D. Gregory <ggregory@apache.org>
2015-03-22 17:32:16 +0000

    "Ashcroft" is also coded as A261.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/codec/trunk@1668417 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/commons/codec/language/SoundexTest.java
+++ src/test/java/org/apache/commons/codec/language/SoundexTest.java
@@ -227,6 +227,7 @@ public class SoundexTest extends StringEncoderAbstractTest<Soundex> {
         // Ashcraft is coded A-261 (A, 2 for the S, C ignored, 6 for the R, 1
         // for the F). It is not coded A-226.
         Assert.assertEquals("A261", this.getStringEncoder().encode("Ashcraft"));
+        Assert.assertEquals("A261", this.getStringEncoder().encode("Ashcroft"));
     }
 
     /**
