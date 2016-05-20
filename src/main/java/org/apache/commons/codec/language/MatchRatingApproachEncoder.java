68ae60d779e3bf4a46064fc1b537057478a627e4
Sebastian Bazley <sebb@apache.org>
2016-05-20 14:21:50 +0000

    Unused
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/codec/trunk@1744748 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/codec/language/MatchRatingApproachEncoder.java
+++ src/main/java/org/apache/commons/codec/language/MatchRatingApproachEncoder.java
@@ -38,7 +38,7 @@ public class MatchRatingApproachEncoder implements StringEncoder {
     /**
      * Constants used mainly for the min rating value.
      */
-    private static final int ONE = 1, TWO = 2, THREE = 3, FOUR = 4, FIVE = 5, SIX = 6, SEVEN = 7, EIGHT = 8,
+    private static final int ONE = 1, TWO = 2, THREE = 3, FOUR = 4, FIVE = 5, SIX = 6, SEVEN = 7,
                              ELEVEN = 11, TWELVE = 12;
 
     /**
