87da2161026eed0f209589973a46925d5e0f1dd4
Gary D. Gregory <ggregory@apache.org>
2017-10-10 23:19:32 +0000

    Do not use an unknown Javadoc tag, this make Javdoc 8 blow up.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/codec/trunk@1811782 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/codec/digest/XXHash32.java
+++ src/main/java/org/apache/commons/codec/digest/XXHash32.java
@@ -27,9 +27,8 @@ import java.util.zip.Checksum;
  *
  * <p>Copied from Commons Compress 1.14
  * <a href="https://git-wip-us.apache.org/repos/asf?p=commons-compress.git;a=blob;f=src/main/java/org/apache/commons/compress/compressors/lz4/XXHash32.java;h=a406ffc197449be594d46f0d2712b2d4786a1e68;hb=HEAD">https://git-wip-us.apache.org/repos/asf?p=commons-compress.git;a=blob;f=src/main/java/org/apache/commons/compress/compressors/lz4/XXHash32.java;h=a406ffc197449be594d46f0d2712b2d4786a1e68;hb=HEAD</a></p>
- *
+ * <p>NotThreadSafe</p>
  * @see <a href="http://cyan4973.github.io/xxHash/">xxHash</a>
- * @NotThreadSafe
  * @since 1.11
  */
 public class XXHash32 implements Checksum {
