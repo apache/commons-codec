599bedc4e8cc7e997b947c488e322951a3e53a4e
Gary Gregory <garydgregory@gmail.com>
2023-08-21 17:12:23 -0400

    Fix formating

--- src/main/java/org/apache/commons/codec/language/Metaphone.java
+++ src/main/java/org/apache/commons/codec/language/Metaphone.java
@@ -312,7 +312,7 @@ public class Metaphone implements StringEncoder {
 
     private boolean isPreviousChar(final StringBuilder string, final int index, final char c) {
         boolean matches = false;
-        if( index > 0 &&
+        if (index > 0 &&
             index < string.length() ) {
             matches = string.charAt(index - 1) == c;
         }
@@ -321,7 +321,7 @@ public class Metaphone implements StringEncoder {
 
     private boolean isNextChar(final StringBuilder string, final int index, final char c) {
         boolean matches = false;
-        if( index >= 0 &&
+        if (index >= 0 &&
             index < string.length() - 1 ) {
             matches = string.charAt(index + 1) == c;
         }
