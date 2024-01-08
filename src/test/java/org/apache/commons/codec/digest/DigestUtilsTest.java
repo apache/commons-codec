4b293eb324469821172d9a4ff0c71885eb451b3f
TestImprove <testimprove41@gmail.com>
2024-01-08 04:53:18 -0600

    modifying data size in DigestUtilsTest to speed up MessageDigestAlgorithmsTest

--- src/test/java/org/apache/commons/codec/digest/DigestUtilsTest.java
+++ src/test/java/org/apache/commons/codec/digest/DigestUtilsTest.java
@@ -56,7 +56,7 @@ public class DigestUtilsTest {
 
     private static final String EMPTY_STRING = "";
 
-    private final byte[] testData = new byte[1024 * 1024];
+    private final byte[] testData = new byte[32 * 32];
 
     private Path testFile;
 
