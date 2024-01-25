19a6efb94c2df1503af0a03827846621d5a9bd61
Gary Gregory <garydgregory@gmail.com>
2024-01-25 12:01:47 -0500

    Deprecate StringUtils 0-argument constructor

--- src/main/java/org/apache/commons/codec/binary/StringUtils.java
+++ src/main/java/org/apache/commons/codec/binary/StringUtils.java
@@ -412,4 +412,13 @@ public class StringUtils {
         return newString(bytes, StandardCharsets.UTF_8);
     }
 
+    /**
+     * TODO Make private in 2.0.
+     *
+     * @deprecated TODO Make private in 2.0.
+     */
+    @Deprecated
+    public StringUtils() {
+        // empty
+    }
 }
