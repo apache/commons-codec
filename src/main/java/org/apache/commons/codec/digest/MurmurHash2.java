36cc67fd3e3e98e4df46e6aa63e104a9485f3124
Melissa OuldBraham <melissaobraham@gmail.com>
2024-02-04 15:31:59 +0100

    adding comments

--- src/main/java/org/apache/commons/codec/digest/MurmurHash2.java
+++ src/main/java/org/apache/commons/codec/digest/MurmurHash2.java
@@ -139,11 +139,14 @@ public final class MurmurHash2 {
         switch (length - index) {
         case 3:
             h ^= (data[index + 2] & 0xff) << 16;
+            // fall through
         case 2:
             h ^= (data[index + 1] & 0xff) << 8;
+            // fall through
         case 1:
             h ^= data[index] & 0xff;
-            h *= M32; break;
+            h *= M32;
+            break;
         default: break;
         }
 
@@ -248,19 +251,26 @@ public final class MurmurHash2 {
         switch (length - index) {
         case 7:
             h ^= ((long) data[index + 6] & 0xff) << 48;
+            // fall through
         case 6:
             h ^= ((long) data[index + 5] & 0xff) << 40;
+            // fall through
         case 5:
             h ^= ((long) data[index + 4] & 0xff) << 32;
+            // fall through
         case 4:
             h ^= ((long) data[index + 3] & 0xff) << 24;
+            // fall through
         case 3:
             h ^= ((long) data[index + 2] & 0xff) << 16;
+            // fall through
         case 2:
             h ^= ((long) data[index + 1] & 0xff) << 8;
+            // fall through
         case 1:
             h ^= (long) data[index] & 0xff;
-            h *= M64; break;
+            h *= M64;
+            break;
         default:
             break;
         }
