0da3d3f2ed975404c04e1907417c70cdeefc5eb3
Gary Gregory <garydgregory@gmail.com>
2024-03-28 09:16:59 -0400

    Javadoc

--- src/test/java/org/apache/commons/codec/digest/DigestUtilsTest.java
+++ src/test/java/org/apache/commons/codec/digest/DigestUtilsTest.java
@@ -50,7 +50,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 /**
- * Tests DigestUtils methods.
+ * Tests {@link DigestUtils}.
  */
 public class DigestUtilsTest {
 
