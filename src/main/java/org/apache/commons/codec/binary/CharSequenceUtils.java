35c9e80599053c32ed3a77eb7791fe8fb1fd1e98
Gary Gregory <garydgregory@gmail.com>
2024-04-19 07:58:29 -0400

    Whitespace

--- src/main/java/org/apache/commons/codec/binary/CharSequenceUtils.java
+++ src/main/java/org/apache/commons/codec/binary/CharSequenceUtils.java
@@ -59,26 +59,21 @@ public class CharSequenceUtils {
         int index1 = thisStart;
         int index2 = start;
         int tmpLen = length;
-
         while (tmpLen-- > 0) {
             final char c1 = cs.charAt(index1++);
             final char c2 = substring.charAt(index2++);
-
             if (c1 == c2) {
                 continue;
             }
-
             if (!ignoreCase) {
                 return false;
             }
-
             // The same check as in String.regionMatches():
             if (Character.toUpperCase(c1) != Character.toUpperCase(c2) &&
                     Character.toLowerCase(c1) != Character.toLowerCase(c2)) {
                 return false;
             }
         }
-
         return true;
     }
 }
