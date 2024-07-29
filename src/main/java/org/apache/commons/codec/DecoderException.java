867eafc0877d3c78dd59b6721c78a977dd0245fe
Gary Gregory <garydgregory@gmail.com>
2024-07-29 07:58:36 -0400

    Use Javadoc @code

--- src/main/java/org/apache/commons/codec/DecoderException.java
+++ src/main/java/org/apache/commons/codec/DecoderException.java
@@ -69,8 +69,8 @@ public class DecoderException extends Exception {
     }
 
     /**
-     * Constructs a new exception with the specified cause and a detail message of <code>(cause==null ?
-     * null : cause.toString())</code> (which typically contains the class and detail message of {@code cause}).
+     * Constructs a new exception with the specified cause and a detail message of {@code (cause==null ?
+     * null : cause.toString())} (which typically contains the class and detail message of {@code cause}).
      * This constructor is useful for exceptions that are little more than wrappers for other throwables.
      *
      * @param cause
