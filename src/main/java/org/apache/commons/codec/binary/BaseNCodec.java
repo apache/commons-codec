8c7fcfe178832702f837c4211ef02263073a4d4f
Gary D. Gregory <garydgregory@gmail.com>
2025-07-19 07:33:52 -0400

    Javadoc

--- src/main/java/org/apache/commons/codec/binary/BaseNCodec.java
+++ src/main/java/org/apache/commons/codec/binary/BaseNCodec.java
@@ -388,10 +388,15 @@ public abstract class BaseNCodec implements BinaryEncoder, BinaryDecoder {
     }
 
     /**
+     * Deprecated: Will be removed in 2.0.
+     * <p>
+     * instance variable just in case it needs to vary later
+     * </p>
+     *
      * @deprecated Use {@link #pad}. Will be removed in 2.0.
      */
     @Deprecated
-    protected final byte PAD = PAD_DEFAULT; // instance variable just in case it needs to vary later
+    protected final byte PAD = PAD_DEFAULT;
 
     /** Pad byte. Instance variable just in case it needs to vary later. */
     protected final byte pad;
