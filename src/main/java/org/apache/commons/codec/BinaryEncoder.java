99c53ffb770833ef2b3fce19da15572e3e17c377
Gary Gregory <garydgregory@gmail.com>
2025-12-31 09:21:18 -0500

    Javadoc

--- src/main/java/org/apache/commons/codec/BinaryEncoder.java
+++ src/main/java/org/apache/commons/codec/BinaryEncoder.java
@@ -25,12 +25,9 @@ public interface BinaryEncoder extends Encoder {
     /**
      * Encodes a byte array and return the encoded data as a byte array.
      *
-     * @param source
-     *            Data to be encoded
-     * @return A byte array containing the encoded data
-     * @throws EncoderException
-     *             thrown if the Encoder encounters a failure condition during the encoding process.
+     * @param source Data to be encoded.
+     * @return A byte array containing the encoded data.
+     * @throws EncoderException thrown if the Encoder encounters a failure condition during the encoding process.
      */
     byte[] encode(byte[] source) throws EncoderException;
 }
-
